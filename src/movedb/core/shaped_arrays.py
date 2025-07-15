230a0c53d7f53dcd348aa9bd3fcc36fb603ff83a
Hudson Burke <hpb7kr@virginia.edu>
2025-07-15 03:28:45 -0400

    Fix numpydantic types

--- src/movedb/core/shaped_arrays.py
+++ src/movedb/core/shaped_arrays.py
@@ -1,16 +1,14 @@
 from numpydantic import NDArray, Shape
 
-Vector3D = NDArray[Shape["3"]]  # pyright: ignore[reportInvalidTypeArguments]
+Vector3D = NDArray[Shape["3"], float]  # pyright: ignore[reportInvalidTypeArguments]
 
-Matrix6x6 = NDArray[Shape["6, 6"]]  # pyright: ignore[reportInvalidTypeArguments]
-Corners3x4 = NDArray[Shape["3, 4"]]  # pyright: ignore[reportInvalidTypeArguments]
+Matrix6x6 = NDArray[Shape["6, 6"], float]  # pyright: ignore[reportInvalidTypeArguments]
+Corners3x4 = NDArray[Shape["3, 4"], float]  # pyright: ignore[reportInvalidTypeArguments]
 
 
 # General matrix types
-MatrixNx3 = NDArray[Shape["*, 3"]]  # pyright: ignore[reportInvalidTypeArguments]
-Matrix3x3 = NDArray[Shape["3, 3"]]  # pyright: ignore[reportInvalidTypeArguments]
-Matrix3xN = NDArray[Shape["3, *"]]  # pyright: ignore[reportInvalidTypeArguments]
+MatrixNx3 = NDArray[Shape["*, 3"], float]  # pyright: ignore[reportInvalidTypeArguments]
+Matrix3x3 = NDArray[Shape["3, 3"], float]  # pyright: ignore[reportInvalidTypeArguments]
+Matrix3xN = NDArray[Shape["3, *"], float]  # pyright: ignore[reportInvalidTypeArguments]
 
-CartesianTrajectory = NDArray[
-    Shape["*, 3 xyz"]
-]  # pyright: ignore[reportInvalidTypeArguments]
+CartesianTrajectory = NDArray[Shape["*, 3 xyz"], float]  # pyright: ignore[reportInvalidTypeArguments]
